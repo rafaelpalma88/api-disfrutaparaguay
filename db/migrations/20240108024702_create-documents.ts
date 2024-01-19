06fd08fb44b9ad797ffa20109f020cfeb973647c
Rafael Costa Palma <rafaelcostapalma@protonmail.com>
2024-01-19 09:54:39 -0300

    changing migrations

--- db/migrations/20240108024702_create-documents.ts
+++ db/migrations/20240108024702_create-documents.ts
@@ -1,14 +1,18 @@
 import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTable("transactions", (table) => {
-    table.uuid("id").primary();
-    table.text("title").notNullable();
-    table.decimal("amount", 10, 2).notNullable();
-    table.timestamp("created_at").defaultTo(knex.fn.now()).notNullable();
-  });
+  const tableExists = await knex.schema.hasTable("transactions");
+
+  if (!tableExists) {
+    await knex.schema.createTable("transactions", (table) => {
+      table.uuid("id").primary();
+      table.text("title").notNullable();
+      table.decimal("amount", 10, 2).notNullable();
+      table.timestamp("created_at").defaultTo(knex.fn.now()).notNullable();
+    });
+  }
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTable("transactions");
+  await knex.schema.dropTableIfExists("transactions");
 }

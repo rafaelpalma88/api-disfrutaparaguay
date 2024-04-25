1f6f739b19b5971aebea8819de065ce5593e94ae
Rafael Costa Palma <rafaelcostapalma@protonmail.com>
2024-04-25 15:02:41 -0300

    removing supertest

--- src/middlewares/check-session-id-exists.ts
+++ src/middlewares/check-session-id-exists.ts
@@ -1,14 +1,14 @@
-import { type FastifyReply, type FastifyRequest } from "fastify";
+// import { type FastifyReply, type FastifyRequest } from "fastify";
 
-export async function checkSessionIdExists(
-  request: FastifyRequest,
-  reply: FastifyReply,
-): Promise<any> {
-  const { sessionId } = request.cookies;
+// export async function checkSessionIdExists(
+//   request: FastifyRequest,
+//   reply: FastifyReply,
+// ): Promise<any> {
+//   const { sessionId } = request.cookies;
 
-  if (sessionId == null) {
-    return await reply.status(401).send({
-      error: "Unauthorized",
-    });
-  }
-}
+//   if (sessionId == null) {
+//     return await reply.status(401).send({
+//       error: "Unauthorized",
+//     });
+//   }
+// }

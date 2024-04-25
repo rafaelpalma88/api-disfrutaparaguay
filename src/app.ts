3a6bd6ee695d41eeee18e8e2f0daa80a459c2a8b
Rafael Costa Palma <rafaelcostapalma@protonmail.com>
2024-04-25 13:38:56 -0300

    implementing error handler

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,24 @@
 import fastify from "fastify";
 import { appRoutes } from "./http/routes";
 import { ZodError } from "zod";
+import { env } from "./env";
 
 export const app = fastify();
 
 app.register(appRoutes);
 
-app.setErrorHandler((error, request, reply) => {
-  console.log("reply", reply);
+app.setErrorHandler((error, _request, reply) => {
   if (error instanceof ZodError) {
-    console.log("caimos aqui");
     return reply
       .status(400)
       .send({ message: "Validation error", issues: error.format() });
   }
+
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
+  } else {
+    // Log externo para uma ferramenta como DataDog/ New Relic / Sentry etc.
+  }
+
+  return reply.status(500).send({ message: "Internal server error" });
 });

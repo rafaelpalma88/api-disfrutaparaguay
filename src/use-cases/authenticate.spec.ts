2615b662aa926eefae402560f2d735b649a5e914
Rafael Costa Palma <rafaelcostapalma@protonmail.com>
2024-04-26 11:38:04 -0300

    implementing authentication tests

---
+++ src/use-cases/authenticate.spec.ts
@@ -0,0 +1,58 @@
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
+import { beforeEach, describe, expect, it } from "vitest";
+import { AuthenticateUseCase } from "./authenticate";
+import { hash } from "bcryptjs";
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
+
+let usersRepository: InMemoryUsersRepository;
+let authenticateUseCase: AuthenticateUseCase;
+
+describe("Use Cases: Authenticate", () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+
+    authenticateUseCase = new AuthenticateUseCase(usersRepository);
+  });
+  it("should return a user if login infos are correct", async () => {
+    await usersRepository.create({
+      name: "John Doe",
+      password_hash: await hash("test@123", 6),
+      email: "john@doe.com",
+    });
+
+    const { user } = await authenticateUseCase.execute({
+      password: "test@123",
+      email: "john@doe.com",
+    });
+
+    expect(user.id).toEqual(expect.any(String));
+  });
+  it("should return an error if password is incorrect", async () => {
+    await usersRepository.create({
+      name: "John Doe",
+      password_hash: await hash("test@123", 6),
+      email: "john@doe.com",
+    });
+
+    await expect(() =>
+      authenticateUseCase.execute({
+        password: "test@000",
+        email: "john@doe.com",
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError);
+  });
+  it("should return an error if email is incorrect", async () => {
+    await usersRepository.create({
+      name: "John Doe",
+      password_hash: await hash("test@123", 6),
+      email: "john@doe.com",
+    });
+
+    await expect(() =>
+      authenticateUseCase.execute({
+        password: "test@123",
+        email: "xxx@doe.com",
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError);
+  });
+});

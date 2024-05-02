e2e6a87b61a77a6b8b9b9346feb510edb04f0fbf
Rafael Costa Palma <rafaelcostapalma@protonmail.com>
2024-05-02 09:02:16 -0300

    correcting import jwt

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import fastify from "fastify";
 import { appRoutes } from "./http/routes";
 import { ZodError } from "zod";
 import { env } from "./env";
-import { fastifyJwt } from "@fastify/jwt";
+import fastifyJwt from "@fastify/jwt";
 
 export const app = fastify();
 

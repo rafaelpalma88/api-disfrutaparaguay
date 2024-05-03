68908d8f45a43485346900035c19064238f7e896
Rafael Costa Palma <rafaelcostapalma@protonmail.com>
2024-05-03 09:30:14 -0300

    including fastify jwt dependency

--- src/@types/fastify-jwt.d.ts
+++ src/@types/fastify-jwt.d.ts
@@ -1,13 +1,9 @@
-// fastify-jwt.d.ts
 import "@fastify/jwt";
 
 declare module "@fastify/jwt" {
   export interface FastifyJWT {
-    // payload: { id: number }; // payload type is used for signing and verifying
     user: {
-      // id: number;
       sub: string;
-      // age: number
-    }; // user type is return type of `request.user` object
+    };
   }
 }

c24f9649cb9e4025177e41309a14386b0aaf9371
Rafael Costa Palma <rafaelcostapalma@protonmail.com>
2024-05-03 10:41:42 -0300

    changing jwt request

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,11 @@ import fastify from "fastify";
 import { usersRoutes } from "./http/controllers/users/routes";
 import { ZodError } from "zod";
 import { env } from "./env";
+import fastifyJwt from "@fastify/jwt";
 
 export const app = fastify();
 
-app.register(require("@fastify/jwt"), {
+app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
 });
 
